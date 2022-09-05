c0525c5b5b2a023a9567281a6aeae7bd618bb485
Ismatik <mukhamejanov.ismat@gmail.com>
2022-09-05 15:51:12 +0600

    Made addition.

--- pd_test.py
+++ pd_test.py
@@ -1,8 +1,23 @@
 class PythonTest:
-    pass
+    def __init__(self , filename) -> None:
+        self.file = filename
+
+#Here we will ask user to say what action they want to do and call 
+    def action(self , action):
+        self.action = action
+
+        if(self.action == "Add"):
+            self.action_add()
+
+    def action_add(self):
+        text = input("What product you want to add?(Please follow instruction Food - Price")
+        with open(self.file , "a+") as file:
+            file.write("\n")
+            file.write(text)
 
 
 
 #What actions can be done: Add to file, Make changes in list, Delete from list, Overall sum.
 action = "Add"
-test = PythonTest("filename.csv" , action)
+test = PythonTest("filename.csv")
+test.action(action)
