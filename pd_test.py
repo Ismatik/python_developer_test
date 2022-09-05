7f24a96b9c3183cebb5b25473d75574ed39efbff
Ismatik <mukhamejanov.ismat@gmail.com>
2022-09-05 15:10:33 +0600

    Setting up what should be done

--- pd_test.py
+++ pd_test.py
@@ -1,2 +1,8 @@
 class PythonTest:
     pass
+
+
+
+#What actions can be done: Add to file, Make changes in list, Delete from list, Overall sum.
+action = "Add"
+test = PythonTest("filename.csv" , action)
