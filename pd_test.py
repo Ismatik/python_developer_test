442d2a6fac1d4fe96d6abe0265fc420c3339f1df
Ismatik <mukhamejanov.ismat@gmail.com>
2022-09-06 14:30:55 +0600

    Calculation overall sum of prices.

--- pd_test.py
+++ pd_test.py
@@ -1,3 +1,6 @@
+#For searching price in the list
+import re
+
 class PythonTest:
     def __init__(self , filename , action) -> None:
         self.file = filename
@@ -16,14 +19,18 @@ class PythonTest:
 #Delete
         if(self.action_v == "Delete"):
             self.action_delete()
-        
+#Sum
+        if(self.action_v == "Sum"):
+            self.action_sum()
+
+
 
     def action_add(self):
-        text = input("What product you want to add?(Please follow instruction Food - Price")
+        text = input("What product you want to add?(Please follow instruction *Food - Price*): ")
         
         with open(self.file , "a+") as file:
-            file.write("\n")
-            file.write(text)
+            # file.write("\n")
+            file.write(text + "\n")
         file.close()
 
     def action_change(self):
@@ -44,6 +51,7 @@ class PythonTest:
 
     def action_delete(self):
         text = input("Which food you want to remove?")
+
         with open(self.file , "r") as file:
             lines = file.readlines()
         
@@ -52,12 +60,24 @@ class PythonTest:
                 if text not in line:
                     file.write(line)
 
-    
-            
+    def action_sum(self):
+        sum = 0
+        with open(self.file , "r") as file:
+            for line in file.readlines():
+                result = line[:-1].split("-")
+                sum += int(result[-1])
                 
+            # for line in lines:
+            #     print(line)
+            #     if(line == " "):
+            #         continue
+            #     sum += int((re.search("- (.*)\n" , line)).group(1))
+
+        print(f"Overall sum of the products in your list is : {sum}")        
 
 
-#What actions can be done: Add to file, Make changes in list, Delete from list, Overall sum.
+#What actions can be done: Add to file, Make changes in list, Delete from list, Sum of the products.
+file_name = input("Please enter name of the file: ")
 action_1 = input("What action you want to perform? \nAdd , Change, Delete, Overall sum - ")
-test = PythonTest("filename.csv" , action_1)
+test = PythonTest(file_name , action_1)
 #test.action(action)
